02fae87a98ca4bdb65775d904826ee85f96f12b7
gyangying <gyangying@163.com>
2020-02-06 20:48:27 +0800

    'Login模块完成'

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+import axios from 'axios'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from '@/router'
 
+Vue.prototype.$http = axios
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,69 @@
 <template>
   <div class="container-login">
     <el-card class="my-card">
-      <img src="../../assets/bg1.jpg" alt />
+      <img src="../../assets/images/logo_index.png" alt />
+      <!-- 表单 -->
+      <el-form ref="loginForm" :model="loginForm" :rules="loginRules" status-icon>
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入验证码"
+            style="width:240px;margin-right:7px"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="login()" type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: "app-login"
-};
+  name: 'app-login',
+  data () {
+    const checkMobile = (rule, value, callback) => {
+      if (!/^1[3-9]\d{9}/.test(value)) {
+        callback(new Error('手机格式不对'))
+      } else {
+        callback()
+      }      
+    }
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '请输入6位验证码', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$ref.loginForm.validate((valid) => {
+        if (valid) {
+          
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style  scoped lang='less'>
@@ -19,7 +73,7 @@ export default {
   position: absolute;
   top: 0;
   left: 0;
-  background: url(../../assets/bg1.jpg) no-repeat center/cover;
+  background: url(../../assets/images/login_bg.jpg) no-repeat center/cover;
   // background-size: cover;
   .my-card {
     width: 400px;
