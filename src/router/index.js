4677fe94cdf1a8d7ad37e7868b959f926c1dbfa9
gyangying <gyangying@163.com>
2020-02-06 12:50:22 +0800

    '登录布局'

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,12 @@
 import VueRouter from 'vue-router'
+import Vue from 'vue'
+import Login from '@/views/login'
+
 Vue.use(VueRouter)
+
 const router = new VueRouter({
-  routers:[]
+  routes: [
+    { path: '/login', component: Login }
+  ]
 })
+export default router
---
+++ src/views/login/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container-login">
+    <el-card class="my-card">
+      <img src="../../assets/bg1.jpg" alt />
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app-login"
+};
+</script>
+
+<style  scoped lang='less'>
+.container-login {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: url(../../assets/bg1.jpg) no-repeat center/cover;
+  // background-size: cover;
+  .my-card {
+    width: 400px;
+    height: 350px;
+    transform: translate(-50%, -60%);
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    img {
+      width: 200px;
+      display: block;
+      margin: 0 auto 20px;
+    }
+  }
+}
+</style>
